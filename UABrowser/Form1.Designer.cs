70e149984b1a7b277ecfc81adedde9e6c7323b72
想飞的水 <pengyibo2009@126.com>
2017-09-22 13:54:41 +0800

    Add files via upload

---
+++ UABrowser/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace UABrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUa = new System.Windows.Forms.Label();
+            this.comboBoxUa = new System.Windows.Forms.ComboBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.textBoxUa = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.labelUrl = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.checkBoxUaSet = new System.Windows.Forms.CheckBox();
+            this.buttonQrScan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUa
+            // 
+            this.labelUa.AutoSize = true;
+            this.labelUa.Location = new System.Drawing.Point(41, 52);
+            this.labelUa.Name = "labelUa";
+            this.labelUa.Size = new System.Drawing.Size(53, 12);
+            this.labelUa.TabIndex = 0;
+            this.labelUa.Text = "打开方式";
+            // 
+            // comboBoxUa
+            // 
+            this.comboBoxUa.FormattingEnabled = true;
+            this.comboBoxUa.Location = new System.Drawing.Point(99, 49);
+            this.comboBoxUa.Name = "comboBoxUa";
+            this.comboBoxUa.Size = new System.Drawing.Size(172, 20);
+            this.comboBoxUa.TabIndex = 1;
+            this.comboBoxUa.SelectedIndexChanged += new System.EventHandler(this.comboBoxUa_SelectedIndexChanged);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(294, 49);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "刷新";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // textBoxUa
+            // 
+            this.textBoxUa.Location = new System.Drawing.Point(375, 49);
+            this.textBoxUa.Name = "textBoxUa";
+            this.textBoxUa.Size = new System.Drawing.Size(473, 21);
+            this.textBoxUa.TabIndex = 3;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(43, 92);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(931, 420);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.Location = new System.Drawing.Point(41, 22);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.Size = new System.Drawing.Size(29, 12);
+            this.labelUrl.TabIndex = 5;
+            this.labelUrl.Text = "网址";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(99, 12);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(749, 21);
+            this.textBoxUrl.TabIndex = 6;
+            this.textBoxUrl.TextChanged += new System.EventHandler(this.textBoxUrl_TextChanged);
+            // 
+            // checkBoxUaSet
+            // 
+            this.checkBoxUaSet.AutoSize = true;
+            this.checkBoxUaSet.Location = new System.Drawing.Point(878, 54);
+            this.checkBoxUaSet.Name = "checkBoxUaSet";
+            this.checkBoxUaSet.Size = new System.Drawing.Size(108, 16);
+            this.checkBoxUaSet.TabIndex = 7;
+            this.checkBoxUaSet.Text = "启用自定义Ustr";
+            this.checkBoxUaSet.UseVisualStyleBackColor = true;
+            this.checkBoxUaSet.CheckedChanged += new System.EventHandler(this.checkBoxUaSet_CheckedChanged);
+            // 
+            // buttonQrScan
+            // 
+            this.buttonQrScan.Location = new System.Drawing.Point(867, 17);
+            this.buttonQrScan.Name = "buttonQrScan";
+            this.buttonQrScan.Size = new System.Drawing.Size(107, 23);
+            this.buttonQrScan.TabIndex = 8;
+            this.buttonQrScan.Text = "解析二维码图片";
+            this.buttonQrScan.UseVisualStyleBackColor = true;
+            this.buttonQrScan.Click += new System.EventHandler(this.buttonQrScan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 545);
+            this.Controls.Add(this.buttonQrScan);
+            this.Controls.Add(this.checkBoxUaSet);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.labelUrl);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBoxUa);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.comboBoxUa);
+            this.Controls.Add(this.labelUa);
+            this.Name = "Form1";
+            this.Text = "U浏览器---我能瞬间变身微信  Author：YB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUa;
+        private System.Windows.Forms.ComboBox comboBoxUa;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox textBoxUa;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label labelUrl;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.CheckBox checkBoxUaSet;
+        private System.Windows.Forms.Button buttonQrScan;
+    }
+}
+
---
+++ UABrowser/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Spire.Barcode;
+
+namespace UABrowser
+{
+    //public delegate 
+    public partial class Form1 : Form
+    {
+        string UaStr; //全局UA标识
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxUrl.Text = "http://m.baidu.com";
+            comboBoxUa.Items.Add("微信内置浏览器-Android");
+            comboBoxUa.Items.Add("微信内置浏览器-iPhone");
+            comboBoxUa.SelectedIndex = 0;
+            textBoxUa.Enabled = false;
+        }
+
+        private void textBoxUrl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        [DllImport("urlmon.dll", CharSet = CharSet.Ansi)]
+        private static extern int UrlMkSetSessionOption(int dwOption, string pBuffer, int dwBufferLength, int dwReserved);
+        const int URLMON_OPTION_USERAGENT = 0x10000001;
+        private void ChangeUserAgent(string Agent)
+        {
+            UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, Agent, Agent.Length, 0);
+        }
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = false;
+            if (textBoxUrl.Text != "")
+            {
+                while (webBrowser1.IsBusy) Application.DoEvents();
+                //修改UA
+                //ChangeUserAgent(UaStr);
+                //Navigate方法重载
+                webBrowser1.Navigate(textBoxUrl.Text);
+                    //, "_self", null, "User-Agent:Mozilla / 5.0(Linux; Android 4.4.4; HM NOTE 1LTEW Build/ KTU84P) AppleWebKit / 537.36(KHTML, like Gecko) Version / 4.0 Chrome / 33.0.0.0 Mobile Safari/ 537.36 MicroMessenger / 6.0.0.54_r849063.501 NetType / WIFI");
+            }
+        }
+
+        private void comboBoxUa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxUa.SelectedIndex)
+            {
+                case 0: ChangeUserAgent("Mozilla/5.0 (Linux; Android 4.4.4; HM NOTE 1LTEW Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36 MicroMessenger/6.0.0.54_r849063.501 NetType/WIFI");break;
+                case 1: ChangeUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12A365 MicroMessenger/5.4.1 NetType/WIFI");break;
+            }
+        }
+
+        private void checkBoxUaSet_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxUaSet.Checked)
+            {
+                comboBoxUa.Enabled = false;
+                textBoxUa.Enabled = true;
+                if (textBoxUa.Text != "")
+                {
+                    ChangeUserAgent(textBoxUa.Text);
+                }
+                else
+                {
+                    MessageBox.Show("粘贴UA到框中");
+                }
+            }
+            else
+            {
+                comboBoxUa.Enabled = true;
+                textBoxUa.Enabled = false;
+            }
+        }
+
+        private void buttonQrScan_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] datas = BarcodeScanner.Scan(ofd.FileName);
+                textBoxUrl.Text = datas[0];  //取字符串数组第一个
+            }
+            
+        }
+    }
+}
